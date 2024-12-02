2480c8b733ea49e5d025438ea5df855946b6de25
vincent <vincent@Vincent>
2024-12-02 23:31:56 +0800

    enum_macro

--- examples/enum_macro.rs
+++ examples/enum_macro.rs
@@ -1,17 +1,25 @@
 use macros::EnumFrom;
 
+#[allow(unused)]
 #[derive(EnumFrom, Debug)]
 enum Direction {
   Up(DirectionUp),
   Down,
 }
 
+#[allow(unused)]
 #[derive(Debug)]
 struct DirectionUp {
   speed: u32,
 }
 
 fn main() {
-  let up = Direction::Up(DirectionUp { speed: 42 });
+  let up: Direction = DirectionUp::new(32).into();
   println!("{:?}", up);
+}
+
+impl DirectionUp {
+  fn new(speed: u32) -> Self {
+    DirectionUp { speed }
+  }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,48 @@
 // proc macro crate
-use proc_macro::TokenStream;
 use quote::quote;
 
 // for enum, we'd like to generate From impls for each variant
 #[proc_macro_derive(EnumFrom)]
-pub fn derive_enum_from(input: TokenStream) -> TokenStream {
+pub fn derive_enum_from(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = syn::parse_macro_input!(input as syn::DeriveInput);
     print!("{:#?}", input);
 
-    // get teh ident
+    // get the ident
     let ident = input.ident;
+    // get generics
+    let generics = input.generics;
     // get enum variants
     let variants = match input.data {
-        syn::Data::Enum(e) => e.variants,
-        _ => panic!("EnumFrom can only be derived for enums"),
+        syn::Data::Enum(data) => data.variants,
+        _ => panic!("EnumFrom only works on enums"),
     };
-    // for each variant, get the ident and fileds
-    let (idents, fields): (Vec<_>, Vec<_>) = variants
-        .into_iter()
-        .map(|v| (v.ident, v.fields))
-        .unzip();
-
-    // quote return proc-macro2 TokenStream so we need to convert it to TokenStream
-    quote! {
-        impl From<#ident> for String {
-            fn from(s: #ident) -> String {
-                match s {
-                    #(#idents(f) => f.into(),)*
+    // for each variant, get the ident and fields
+    let from_impls = variants.iter().map(|variant| {
+        let var = &variant.ident;
+        match &variant.fields {
+            syn::Fields::Unnamed(fields) => {
+                // only support one field
+                if fields.unnamed.len() != 1 {
+                    quote! {}
+                } else {
+                    let field = fields.unnamed.first().expect("should have 1 field");
+                    let ty = &field.ty;
+                    quote! {
+                        impl #generics  From<#ty> for #ident #generics {
+                            fn from(v: #ty) -> Self {
+                                #ident::#var(v)
+                            }
+                        }
+                    }
                 }
             }
+
+            _ => quote! {},
         }
+    });
+
+    // quote return proce-macro2 TokenTtream so we need to convert it to TokenStream
+    quote! {
+        #(#from_impls)*
     }.into()
 }
